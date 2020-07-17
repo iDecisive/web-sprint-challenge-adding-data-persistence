773e56b9a10956dbdb29e93d2c7a62a1958ac48f
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-17 11:13:51 -0400

    Setup server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const allProjectsRouter = require('./routers/allProjectsRouter.js');
+
+const server = express();
+
+const PORT = 8000;
+
+server.use(express.json());
+
+server.listen(PORT, () => {
+	console.log(`Server started on port ${PORT}`);
+});
