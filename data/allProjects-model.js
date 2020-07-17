7dab999fde65ad321f3ac9b5578192f8d86387c7
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-17 11:35:25 -0400

    Added a database config file and imported it into a new db model file

---
+++ data/allProjects-model.js
@@ -0,0 +1,16 @@
+const db = require('./allProjectsConfig');
+
+//Adding projects
+
+//Reading projects
+
+//Adding resources
+
+//Reading resources
+
+//Adding tasks
+
+//Reading tasks
+
+//Assigning Resource to a Project
+
